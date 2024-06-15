725b8816bcd8480776ba72a5bc27759f591a6c40
dasunwijayathilaka <maxdasun613@gmail.com>
2024-06-15 16:38:41 +0530

    Initial commit

---
+++ Layered-Architecture-Project/src/main/java/com/example/layeredarchitecture/dao/Ord_DetailDAOImpl.java
@@ -0,0 +1,30 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.OrderDetailDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class Ord_DetailDAOImpl implements Ord_DetailDAO {
+    @Override
+    public boolean saveOrderDetails(List<OrderDetailDTO> orderDetails, String orderId) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement stm = connection.prepareStatement("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
+
+        for (OrderDetailDTO detail : orderDetails) {
+            stm.setString(1, orderId);
+            stm.setString(2, detail.getItemCode());
+            stm.setBigDecimal(3, detail.getUnitPrice());
+            stm.setInt(4, detail.getQty());
+
+        }
+        return stm.executeUpdate() > 0;
+    }
+}
+
+
+            // This code saves order details for multiple items into the database using batch insertion
+            // for efficiency and returns success status.
